4d852b773dc1afd25ec4041a2d4a105764c2c908
gerardduenas <gerardduenas@gmail.com>
2016-07-16 14:03:00 +0200

    Adding code

--- functions.py
+++ functions.py
@@ -5,19 +5,6 @@ import time
 import exceptions as e
 from config import * 
 
-def connect():
-    '''Connects to the server through a socket,
-    identifies the bot to the server and to NickServ'''
-    irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the socket
-    irc.connect((cfg['server'],cfg['port']))
-    irc.send("USER " + cfg['botnick']  + " 8 * :" + cfg['realname'] + "\r\n")
-    name_bot(cfg)
-    irc.send("JOIN "+ cfg['channels'] +"\r\n")
-
-def name_bot(irc):
-    nick = change_nick(irc)
-    irc.send("PRIVMSG nickserv :identify %s %s\r\n" % (cfg['botnick'], cfg['password']))
-
 def change_nick(irc)
     irc.send("NICK " + cfg['botnick'] + "\r\n")
     x = "_"
@@ -31,3 +18,19 @@ def check_config():
         if not y:
             if x not in cfg['optional_cfg']:
                 raise e.ConfigError("Found an error in config.py file")
+                         
+def connect():
+    '''Connects to the server through a socket,
+    identifies the bot to the server and to NickServ'''
+    irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the socket
+    irc.connect((cfg['server'],cfg['port']))
+    irc.send("USER " + cfg['botnick']  + " 8 * :" + cfg['realname'] + "\r\n")
+    name_bot(cfg)
+    irc.send("JOIN "+ cfg['channels'] +"\r\n")
+
+def name_bot(irc):
+    nick = change_nick(irc)
+    irc.send("PRIVMSG nickserv :identify %s %s\r\n" % (cfg['botnick'], cfg['password']))
+    
+def parse_cmd(text):
+    resp = text.split()
